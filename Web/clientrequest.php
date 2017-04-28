f38f3a3f17f144f0eb70e3c1ef6c2c2c6fd82dc2
Elias <riedel50@gmx.de>
2017-04-28 15:26:20 +0200

    copied alredy existig webfiles into new project

---
+++ Web/clientrequest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 13.04.2017
+ * Time: 20:54
+ */
+
+/**
+ * will be called like
+ * /clientrequest.php#h=[kundennummer][lokale ID]...
+ */
+
+$serverip = $_SERVER['SERVER_ADDR'];
+
+?>
+
+<html>
+<head>
+
+</head>
+<body>
+
+</body>
+</html>
---
+++ Web/index.php
@@ -0,0 +1,2 @@
+<?php ?>
+udvide
