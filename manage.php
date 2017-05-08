8e0ebd20223482db4ad40130508e982bb1fb41a4
Elias <riedel50@gmx.de>
2017-05-08 10:23:26 +0200

    new Icons

--- manage.php
+++ manage.php
@@ -3,14 +3,14 @@ $template = file_get_contents('temp/manageTempl.html');
 
     $svg = file_get_contents('res/home.svg');
     $template = str_replace('<!--home-->',$svg,$template);
-    $svg = file_get_contents('res/home.svg');
+    $svg = file_get_contents('res/Entry.svg');
     $template = str_replace('<!--eintrag-->',$svg,$template);
 
 
 $temp = file_get_contents('temp/MitarbeiterTempl.html');
 $template = str_replace('<!--Mitarbeiter-->',$temp,$template);
 
-    $svg = file_get_contents('res/home.svg');
+    $svg = file_get_contents('res/Marker.svg');
     $template = str_replace('<!--marker-->',$svg,$template);
 
 
@@ -18,9 +18,9 @@ $template = str_replace('<!--Mitarbeiter-->',$temp,$template);
 $temp = file_get_contents('temp/AdminTempl.html');
 $template = str_replace('<!--Admin-->',$temp,$template);
 
-    $svg = file_get_contents('res/home.svg');
+    $svg = file_get_contents('res/User.svg');
     $template = str_replace('<!--verwaltung-->',$svg,$template);
-    $svg = file_get_contents('res/home.svg');
+    $svg = file_get_contents('res/Map.svg');
     $template = str_replace('<!--karten-->',$svg,$template);
 
 
