9c5bc5242b3541de458e018749ac3257a84bc1a1
Janssen, Simon <simon.janssen@student.fhws.de>
2017-05-08 10:55:52 +0200

    added gitignore - !!IMPORTANT!! keys.json cannot be public - EVER Lolz will become form recipent or sth like that...

---
+++ LolzImTooTiredForALegitName.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'vuforiaaccess.php';
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 08.05.2017
+ * Time: 10:34
+ */
+class lolzImTooTiredForALegitName
+{
+    public function newTarget()
+    {
+        
+    }
+}
