48331ab242223fa62a85f7e29411ee09db92d128
Elias <riedel50@gmx.de>
2017-05-08 08:34:28 +0200

    splitted up the Templates

--- manage.php
+++ manage.php
@@ -1,19 +1,28 @@
 <?php
 $template = file_get_contents('temp/manageTempl.html');
 
-$temp = file_get_contents('temp/menuTempl.html');
-$template = str_replace('<!--menu-->',$temp,$template);
-
     $svg = file_get_contents('res/home.svg');
     $template = str_replace('<!--home-->',$svg,$template);
     $svg = file_get_contents('res/home.svg');
     $template = str_replace('<!--eintrag-->',$svg,$template);
+
+
+$temp = file_get_contents('temp/MitarbeiterTempl.html');
+$template = str_replace('<!--Mitarbeiter-->',$temp,$template);
+
     $svg = file_get_contents('res/home.svg');
     $template = str_replace('<!--marker-->',$svg,$template);
+
+
+
+$temp = file_get_contents('temp/AdminTempl.html');
+$template = str_replace('<!--Admin-->',$temp,$template);
+
     $svg = file_get_contents('res/home.svg');
     $template = str_replace('<!--verwaltung-->',$svg,$template);
     $svg = file_get_contents('res/home.svg');
     $template = str_replace('<!--karten-->',$svg,$template);
+    
 
 $temp = file_get_contents('temp/contentTempl.html');
 $template = str_replace('<!--content-->',$temp,$template);
