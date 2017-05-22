4e32a9f7cb13d337561ce2f414f1715fef7fccc8
Janssen, Simon <simon.janssen@student.fhws.de>
2017-05-22 19:10:19 +0200

    form create implemented (not yet debugged - need #11 for a nice check) #12 #9 also added txt for spontanous documentation needs

--- crudForm.php
+++ crudForm.php
@@ -20,7 +20,7 @@ class crudFormHandler
         $prevIgnoreUserAbort = ignore_user_abort(true);
         $result = $this->DoNotTouchHandleForm();
         ignore_user_abort($prevIgnoreUserAbort);
-        return $result;
+        return $result === false ? $this->echoMessage : $result;
     }
 
     private function DoNotTouchHandleForm()
@@ -35,98 +35,22 @@ class crudFormHandler
             $perm = getPermissions($this->postData['username'], $this->postData['passHash']);
             // is login invalid? -> Error
             if ($perm === false) {
-                trigger_error('Invalid login (Bad password or username)');
-                return 'Error';
+                $this->echoMessage .= 'Invalid login (Bad password or username)';
+                return false;
             }
 
             // is client or admin? (->editPerm)
             $editPerm = $perm[0] > 0; // true when Admin or Client
+
             // create if $editPerm
             if ($this->postData['udvideVerb'] == 'create' && $editPerm) {
-                do {
-                    $retry = false;
-                    try {
-                        $this->vwsRequest = (new access_vfc())
-                            ->setTargetName($this->postData['t_name'])
-                            ->setImage($this->postData['t_image'])
-                            ->setMeta('/error.php?error=targetNotLinkedToDbYet')// redirect marker to Inform the marker still needs a moment (max 15 min according to API Doc)
-                            ->setActiveflag(false)// Target Inactive until DB synced
-                            ->setAccessMethod('post');
-                        $this->vwsResponse = $this->vwsRequest->execute();
-                    } catch (VuforiaAccessAPIException $e) { // Errors we can potentially fix before the request is actually sent out
-                        if ($e->getCode() < 200) {
-                            switch ($e->getCode()) {
-                                case 110:
-                                    $this->postData['t_name'] = 'Anonymous Target';
-                                    break;
-                                case 111:
-                                    $this->postData['t_name'] = substr($this->postData['t_name'], 0, 60) . '...';
-                                    break;
-                                case 120:
-                                    $this->postData['t_image'] = toJPG($this->postData['t_image'],95);
-                                    break;
-                                default:
-                                    return "Please contact a developer.\n
-                                    While trying to prepare and send your request we encountered unrecognized Error $e->getCode() \n
-                                    with the Message: $e->getMessage() at $e->getLine() in $e->getFile()";
-                            }
-                            $retry = true;
-                            $this->callCounter++;
-                            if ($this->callCounter > 5) {
-                                $this->echoMessage .= var_dump($this->vwsRequest) . " not fixed after trying 5 times.";
-                                return 1;
-                            }
-                        } else if ($e->getCode() > 299) {
-                            return "Please contact a developer.\n
-                            While trying to prepare and send your request we encountered Error $e->getCode() \n
-                            with the Message: $e->getMessage() at $e->getLine() in $e->getFile()";
-                        } else { // User Input not valid?
-                            return "Your request has not been sent! $e->getMessage()";
-                        }
-                    } catch (HttpRequestException $e) {
-                        // ToDo connection error
-                    }
-                } while ($retry);
-
-                $this->callCounter = 0;
-                while ($this->errorHandleVFResponse()===true) {
-                    $this->vwsResponse = $this->vwsRequest->execute();
-                }
-                if ($this->errorHandleVFResponse()===false) {
-                    // ToDo report as unrecoverable
-                }
-
-                $vwsResponseBody = json_decode($this->vwsResponse->getBody());
-
-                // Log Transaction
-                $t_id = $vwsResponseBody['target_id'];
-                $tr_id = $vwsResponseBody['transaction_id'];
-                $sql = "INSERT INTO udvide.TransactionLog VALUES ($tr_id,?,$t_id)";
-                access_DB::prepareExecuteGetStatement($sql, [$this->postData['username']]);
-
-                // Sync DBs
-                $sql = "INSERT INTO udvide.Targets (t_id,t_owner) VALUES ($t_id,?);";
-                access_DB::prepareExecuteGetStatement($sql, [$this->postData['username']]);
-
-                // Update VF DB to the now existing targetpage
-                try {
-                    $this->vwsResponse = (new access_vfc())
-                        ->setTargetId($t_id)
-                        ->setMeta("/clientRequest.php?t=$t_id")
-                        ->setActiveflag($this->postData['activeFlag'])
-                        ->setAccessMethod('put')
-                        ->execute();
-                } catch (VuforiaAccessAPIException $e) {
-
-                } catch (HttpRequestException $e) {
-
-                }
-                $this->errorHandleVFResponse();
-
-                return 'Post successful';
+                return $this->createTarget();
             }
 
-            // ToDo: delete if editPerm
+            // delete if editPerm
+            if ($this->postData['udvideVerb'] == 'delete' && $editPerm) {
+                return $this->deleteTarget();
+            }
 
             // also grant editPerm if Editor for specified marker
             if (!$editPerm) { // true when Editor has Permissions
@@ -137,7 +61,7 @@ class crudFormHandler
                 }
             }
 
-            if ($this->postData['access'] == 'PUT' && $editPerm) { // ToDo Accept more variants? RFC!
+            if ($this->postData['access'] == 'update' && $editPerm) { // ToDo Accept more variants? RFC!
                 $vwsa = new access_vfc();
                 $vwsa->setTargetId($this->postData['t_id']);
 
@@ -156,6 +80,7 @@ class crudFormHandler
                 }
 
                 if ($updateVWS) {
+                    //ToDo refactor to method etc.
                     $vwsResponse = $vwsa->execute('put');
 
                     // ToDo: Error handling etc.
@@ -170,11 +95,12 @@ class crudFormHandler
 
                 return 'Update Successful';
             }
-            return 'Error: Invalid command?';
+            $this->echoMessage .= 'Invalid command?';
+            return false;
         } else {
-            echo "This site is used to evaluate CRUD Form requests.\n
+            $this->echoMessage .= "This site is used to evaluate CRUD Form requests.\n
             Please use the form or consult the Documentation for more information";
-            return 'Error';
+            return false;
         }
     }
 
@@ -200,7 +126,7 @@ class crudFormHandler
                 }
                 break;
             case 404: // UnknownTarget
-                // Target deleted from other source? Sync DB
+                // Target deleted from other source? Sync DB! ToDo
                 break;
             case 422:
                 switch (json_decode($this->vwsResponse->getBody())->status) {
@@ -235,58 +161,155 @@ class crudFormHandler
         return $return; // retry? or null == no error
     }
 
-    /**
-     * @return bool|null
-     */
-    private function errorHandlePreRequest(): bool // ToDo
+    private function errorPrevention()
     {
-        $this->callCounter++;
-        $return = false;
+        // ToDo refactor any easy stuff into here like empty names no jpg images that are
+    }
 
-        switch ($this->vwsResponse->getStatus()) {
-            case 200: // ok
-            case 201: // TargetCreated
-                return null; // no error
-            case 403:
-                switch (json_decode($this->vwsResponse->getBody())->status) {
-                    case 'TargetNameExists':
-                        $this->vwsRequest->setTargetName($this->postData['t_name'].' 2');
-                        $return = false;
-                        break;
+    /**
+     * @return bool
+     */
+    private function createTarget():bool
+    {
+        do {
+            $retry = false;
+            try {
+                $this->vwsRequest = (new access_vfc())
+                    ->setTargetName($this->postData['t_name'])
+                    ->setImage($this->postData['t_image'])
+                    ->setMeta('/error.php?error=targetNotLinkedToDbYet')// redirect marker to Inform the marker still needs a moment (max 15 min according to API Doc)
+                    ->setActiveflag(false)// Target Inactive until DB synced
+                    ->setAccessMethod('create');
+                $this->errorPrevention();
+                $this->vwsResponse = $this->vwsRequest->execute();
+            } catch (VuforiaAccessAPIException $e) { // Errors we can potentially fix before the request is actually sent out
+                if ($this->handleVAAPIE($e)) {
+                    $this->vwsResponse = $this->vwsRequest->execute();
+                } else {
+                    return false;
                 }
-                break;
-            case 404: // UnknownTarget
-                // Target deleted from other source? Sync DB
-                break;
-            case 422:
-                switch (json_decode($this->vwsResponse->getBody())->status) {
-                    case 'BadImage':
-                        //retry: * to JPG conversion (or output)
-                        $return = false;
-                        break;
-                    case 'ImageTooLarge':
-                        // retry /w JPG to JPG conversion (or output)
-                        $img = $this->postData['t_image'];
-                        // ToDo conversion /w file size reduction
-                        $this->vwsRequest->setImage($img);
-                        $return = true;
-                        break;
-                    case 'MetadataTooLarge':
-                        // shouldn't happen... like really -> output
-                        $return = false;
-                        break;
+            } catch (HttpRequestException $e) {
+                $retry = $this->handleHTTPRE($e);
+            }
+            $this->callCounter++;
+            if ($this->callCounter > 5) {
+                $this->echoMessage .= var_dump($this->vwsRequest) . " not fixed after trying 5 times.";
+                return false;
+            }
+        } while ($retry);
+
+        $this->callCounter = 0;
+        while ($this->errorHandleVFResponse() === true) {
+            $this->vwsResponse = $this->vwsRequest->execute();
+        }
+        if ($this->errorHandleVFResponse() === false) {
+            $this->echoMessage .= "Please review your input!";
+            return false;
+        }
+
+        $vwsResponseBody = json_decode($this->vwsResponse->getBody());
+        $t_id = $vwsResponseBody['target_id'];
+        $tr_id = $vwsResponseBody['transaction_id'];
+
+        $this->logTransaction($tr_id,$t_id);
+
+        // Sync DBs and insert our own stuff
+        $sql = "INSERT INTO udvide.Targets (t_id,t_owner,xpos,ypos,map) VALUES ($t_id,?,?,?,?);";
+        access_DB::prepareExecuteGetStatement($sql, [$this->postData['username'], $this->postData['xPos'], $this->postData['yPos'], $this->postData['map']]);
+
+        // Update VF DB to the now named target page
+        do {
+            $retry = false;
+            try {
+                $this->vwsRequest = (new access_vfc())
+                    ->setTargetId($t_id)
+                    ->setMeta("/clientRequest.php?t=$t_id")
+                    ->setActiveflag($this->postData['activeFlag'])
+                    ->setAccessMethod('update');
+                $this->errorPrevention();
+                $this->vwsResponse = $this->vwsRequest->execute();
+            } catch (VuforiaAccessAPIException $e) { // Errors we can potentially fix before the request is actually sent out
+                if ($this->handleVAAPIE($e)) {
+                    $this->vwsResponse = $this->vwsRequest->execute();
+                } else {
+                    return false;
                 }
-                break;
-            case 500: // VuFo Server internal
-                $return = true;
-                break;
-            default:
-                $this->echoMessage .= 'Vuforia Encountered an Error: ' . $this->vwsResponse->getStatus() . $this->vwsResponse->getBody();
+            } catch (HttpRequestException $e) {
+                $retry = $this->handleHTTPRE($e);
+            }
+            $this->callCounter++;
+            if ($this->callCounter > 5) {
+                $this->echoMessage .= var_dump($this->vwsRequest) . " not fixed after trying 5 times.";
+                return false;
+            }
+        } while($retry);
+
+        $this->callCounter = 0;
+        while ($this->errorHandleVFResponse()===true) {
+            $this->vwsResponse = $this->vwsRequest->execute();
         }
-        if ($this->callCounter > 3 && $return) {
-            $this->echoMessage .= var_dump($this->vwsResponse)." not fixed after trying 3 times.";
-            $return = false;
+        if ($this->errorHandleVFResponse()===false) {
+            $this->echoMessage .= "Please review your input!";
+            return false;
         }
-        return $return; // retry? or null == no error
+        $vwsResponseBody = json_decode($this->vwsResponse->getBody());
+        $t_id = $vwsResponseBody['target_id'];
+        $tr_id = $vwsResponseBody['transaction_id'];
+        $this->logTransaction($tr_id,$t_id);
+        return true;
+    }
+
+    /**
+     * @param VuforiaAccessAPIException $e
+     * @return bool
+     */
+    private function handleVAAPIE(VuforiaAccessAPIException $e):bool
+    {
+        if ($e->getCode() < 200) {
+            switch ($e->getCode()) {
+                case 110:
+                    $this->postData['t_name'] = 'Anonymous Target';
+                    break;
+                case 111:
+                    $this->postData['t_name'] = substr($this->postData['t_name'], 0, 60) . '...';
+                    break;
+                case 120:
+                    $this->postData['t_image'] = toJPG($this->postData['t_image'],95);
+                    break;
+                default:
+                    $this->echoMessage .= "Please contact a developer.\n
+                                    While trying to prepare and send your request we encountered unrecognized Error $e->getCode() \n
+                                    with the Message: $e->getMessage() at $e->getLine() in $e->getFile()";
+                    return false;
+            }
+            return true;
+        } else if ($e->getCode() > 299) {
+            $this->echoMessage .= "Please contact a developer.\n
+                            While trying to prepare and send your request we encountered Error $e->getCode() \n
+                            with the Message: $e->getMessage() at $e->getLine() in $e->getFile()";
+        } else { // User Input not valid?
+            $this->echoMessage .= "Your request has not been sent! $e->getMessage()";
+        }
+        return false;
+    }
+
+    /**
+     * @param $e
+     * @return bool
+     */
+    private function handleHTTPRE($e)
+    {
+        throw $e; // Yep you can do that
+        return false; // to calm PHP for the moment
+    }
+
+    /**
+     * @param $tr_id
+     * @param $t_id
+     */
+    private function logTransaction($tr_id, $t_id)
+    {
+        $sql = "INSERT INTO udvide.TransactionLog VALUES ($tr_id,?,$t_id)";
+        access_DB::prepareExecuteGetStatement($sql, [$this->postData['username']]);
     }
 }
