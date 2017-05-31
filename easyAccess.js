c538052aad5948e710459557cf6c8eb12d81453c
Elias <riedel50@gmx.de>
2017-05-31 16:31:04 +0200

    first interactive button #28 (this time for real)

--- easyAccess.js
+++ easyAccess.js
@@ -1,3 +1,17 @@
 /**
  * Created by Elias on 31.05.2017.
  */
+function searchForEntry(){
+
+}
+
+function getEntryTable() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("content").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", "templates/entrytableTempl.html", true);
+    xhttp.send();
+}
