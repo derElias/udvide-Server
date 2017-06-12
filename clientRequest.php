005f052fb7e948dec290daad143e51d37e163cf3
Janssen, Simon <simon.janssen@student.fhws.de>
2017-06-12 16:38:31 +0200

    finally implemented the legendary behemouth of Mobile-App API... 2 LOC

--- clientRequest.php
+++ clientRequest.php
@@ -6,10 +6,11 @@ require_once 'udvide.php';
  * Date: 13.04.2017
  * Time: 20:54
  */
-header('Content-Type: application/json');
+header('Content-Type: application/text');
 /**
  * will be called like
  * /clientRequest.php?t=[kundennummer][lokale ID]...
  */
 $id = $_GET['t'];
-$sql = 'SELECT ';
+$sql = 'SELECT content FROM udvide.Targets WHERE t_id = ?';
+return access_DB::prepareExecuteFetchStatement($sql,[$id])[0]['content'];
