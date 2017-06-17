eb5845e2cfed9296d25a4b4455f21556ad541aa7
Janssen, Simon <simon.janssen@student.fhws.de>
2017-06-17 18:24:06 +0200

    target to its own class (WIP) #38

--- helper.php
+++ helper.php
@@ -16,7 +16,7 @@ require_once 'access_vfc.php';
  * @param array $options supports quality, maxFileSize, doNotCrop, minQuality, minShortestSide
  * @return string|false (smaller) JPG | false on failure
  */
-function jpgAssistant ($img, array $options):string {
+function imgAssistant (&$img, array $options):string {
     // to image resource if not already
     if (is_string($img))
         $img = imagecreatefromstring($img);
@@ -31,6 +31,9 @@ function jpgAssistant ($img, array $options):string {
     // file size reduction mode
     if (isset($options['maxFileSize'])) {
         $maxFSize = $options['maxFileSize'];
+
+        if (imgJpgSize($img) < $maxFSize)
+            return $img;
 
         $doNotCrop = false;
         $minS = 1;
@@ -83,9 +86,10 @@ function jpgAssistant ($img, array $options):string {
     }
 
     // to JPEG string (by capturing output from imagejpeg())
-    $ret = imgResToJpgString($img,$quality);
-    imagedestroy( $img ); // memory cleanup asap
-    return $ret;
+    //$ret = imgResToJpgString($img,$quality);
+    //imagedestroy( $img ); // memory cleanup asap
+
+    return $img;
 }
 
 /**
@@ -129,205 +133,66 @@ function logTransaction($tr_id, $user, $t_id = "no specific")
 }
 
 /**
- * Convert an assoc. array to a target object
- * @param array $array_in associative array with opt: activeFlag map yPos xPos username t_image t_name t_id content
- * @return target
+ * @param string $value
+ * @return string
  */
-function arrayToTarget(array $array_in):target
-{
-    $response = new target();
-    if (isset($array_in['activeFlag'])) {
-        $response->active = $array_in['activeFlag'];
-    }
-    if (isset($array_in['map'])) {
-        $response->map = $array_in['map'];
-    }
-    if (isset($array_in['yPos'])) {
-        $response->yPos = $array_in['yPos'];
-    }
-    if (isset($array_in['xPos'])) {
-        $response->xPos = $array_in['xPos'];
-    }
-    if (isset($array_in['username'])) {
-        $response->owner = $array_in['username'];
-    }
-    if (isset($array_in['t_image'])) {
-        $response->image = $array_in['t_image'];
-    }
-    if (isset($array_in['t_name'])) {
-        $response->name = $array_in['t_name'];
-    }
-    if (isset($array_in['t_id'])) {
-        $response->id = $array_in['t_id'];
-    }
-    if (isset($array_in['content'])) {
-        $response->content = $array_in['content'];
-    }
-    return $response;
+function purifyValue(string $value):string {
+    return htmlspecialchars(stripslashes(trim($value)));
 }
 
 /**
- * Represents a target with all it's keys
- * Class target
+ * @param string $in
+ * @return string
+ * @throws TypeError
  */
-class target {
-    /** @var  string */
-    public $name;
-    /** @var  bool */
-    public $deleted;
-    /** @var  string|resource */
-    public $image;
-    /** @var  bool */
-    public $active;
-    /** @var  string */
-    public $content;
-    /** @var  string */
-    public $owner;
-    /** @var  int */
-    public $xPos;
-    /** @var  int */
-    public $yPos;
-    /** @var  string */
-    public $map;
-    /** @var  int */
-    public $id;
-    /** @var  string */
-    public $vw_id;
+function base64ImgToDecodeAbleBase64(string $in):string {
+    $out = explode(',',$in);
 
-    public function __construct() {}
+    if (!array_key_exists(1,$out) || array_key_exists(2,$out))
+        // A invalid base 64 img input was given
+        throw new TypeError(ERR_TYPE_NOT_BASE64_IMAGE_MSG);
 
-    public static function fromJSON($json = false) {
-        $instance = new self();
-        if ($json) $instance->set(json_decode($json, true));
-        return $instance;
-    }
-
-    public function set($data) {
-        var_dump($data);
-        foreach ($data AS $key => $value) {
-            /*// snippet left from sample for object to custom object conversion
-            if (is_array($value)) {
-                $sub = new self();
-                $sub->set($value);
-                $value = $sub;
-            }//*/
-            $this->{$key} = $value;
-        }
-    }
-
-    //<editor-fold desc="Fluent Setters (set null if omitted param except $deleted)">
-    /**
-     * @param string $name
-     * @return target
-     */
-    public function setName(string $name = null): target
-    {
-        $this->name = $name;
-        return $this;
-    }
-
-    /**
-     * @param resource|string $image
-     * @return target
-     */
-    public function setImage($image = null)
-    {
-        $this->image = $image;
-        return $this;
-    }
-
-    /**
-     * @param bool $active
-     * @return target
-     */
-    public function setActive(bool $active = null): target
-    {
-        $this->active = $active;
-        return $this;
-    }
-
-    /**
-     * @param string $content
-     * @return target
-     */
-    public function setContent(string $content = null): target
-    {
-        $this->content = $content;
-        return $this;
-    }
-
-    /**
-     * @param string $owner
-     * @return target
-     */
-    public function setOwner(string $owner = null): target
-    {
-        $this->owner = $owner;
-        return $this;
-    }
-
-    /**
-     * @param int $xPos
-     * @return target
-     */
-    public function setXPos(int $xPos = null): target
-    {
-        $this->xPos = $xPos;
-        return $this;
-    }
-
-    /**
-     * @param int $yPos
-     * @return target
-     */
-    public function setYPos(int $yPos = null): target
-    {
-        $this->yPos = $yPos;
-        return $this;
-    }
-
-    /**
-     * @param string $map
-     * @return target
-     */
-    public function setMap(string $map = null): target
-    {
-        $this->map = $map;
-        return $this;
-    }
-
-    /**
-     * @param string $id
-     * @return target
-     */
-    public function setId(string $id = null): target
-    {
-        $this->id = $id;
-        return $this;
-    }
-
-    /**
-     * @param string $vw_id
-     * @return target
-     */
-    public function setVwId(string $vw_id = null): target
-    {
-        $this->vw_id = $vw_id;
-        return $this;
-    }
-
-    /**
-     * @param bool $deleted
-     * @return target
-     */
-    public function setDeleted(bool $deleted = false): target
-    {
-        $this->deleted = $deleted;
-        return $this;
-    }
-    //</editor-fold>
+    return $out[1];
 }
 
+/**
+ * Compares a sent in passHash (password) with a peppered and salted passHash
+ * @param string $userPassHash the sent password value (should be hashed client-side)
+ * @param string $serverPassHash the db stored password
+ * @return bool
+ */
+function pepperedPassCheck(string $userPassHash,string $serverPassHash):bool
+{
+    $keys = json_decode(file_get_contents('keys.json'));
+    return password_verify(sha1($userPassHash . $keys->pepper), $serverPassHash);
+}
+
+/**
+ * generates a peppered and salted passHash
+ * @param string $new_users_password
+ * @return bool|string
+ */
+function pepperedPassGen(string $new_users_password)
+{
+    $keys = json_decode(file_get_contents('keys.json'));
+    return password_hash(sha1($new_users_password . $keys->pepper), PASSWORD_DEFAULT);
+}
+
+/**
+ * Based on JPGs magic numbers we can quickly figure out if a file is defiantly not a JPG
+ * false-positives unlikely but possible
+ * false-negatives impossible
+ * @param string $img
+ * @return bool
+ */
+function strIsJpg(string $img):bool {
+    return (ord($img{0}) == 255)
+    && (ord($img{1}) == 216)
+    && (ord($img[strlen($img) - 2]) == 255)
+    && (ord($img[strlen($img) - 1]) == 217);
+}
+
+class LoginException extends Exception {}
 class PermissionException extends Exception {}
 
 class handlerResponse {
--- target.php
+++ target.php
@@ -1,12 +1,367 @@
 <?php
-
+require_once 'helper.php';
 /**
  * Created by PhpStorm.
- * User: User
- * Date: 14.06.2017
+ * User: Simon, all code in this file, except explicitly stated otherwise is written by me
+ * Date: 14.06.2017 Refactor in new file
  * Time: 15:48
  */
 class target
 {
+    // SQL only Values
+    /** @var int */
+    private $id;
+    /** @var  bool */
+    private $deleted;
+    /** @var  string */
+    private $owner;
+    /** @var  string WARNING: UNESCAPED */
+    private $content;
+    /** @var  int */
+    private $xPos;
+    /** @var  int */
+    private $yPos;
+    /** @var  string */
+    private $map;
 
+    // Shared Values
+    /** @var string varchar(32) */
+    private $vw_id;
+    /** @var  resource */
+    private $image;
+
+    // VWS only Values
+    /** @var  string */
+    private $name;
+    /** @var  bool */
+    private $active;
+
+    /**
+     * target constructor.
+     */
+    private function __construct() {}
+
+    /**
+     * indirect constructor
+     * @param string $json
+     * @return target
+     */
+    public static function fromJSON($json = '') {
+        $instance = new self();
+        if (!empty($json))
+            $instance->set(json_decode($json, true));
+        return $instance;
+    }
+
+    /**
+     * indirect constructor
+     * @param array $array
+     * @return target
+     */
+    public static function fromArray(array $array = null) {
+        $instance = new self();
+        if (!empty($array))
+            $instance->set($array);
+        return $instance;
+    }
+
+    /**
+     * indirect constructor
+     * @param null $id
+     * @return target
+     * @internal param bool $array
+     */
+    public static function fromDB($id = null) {
+        $instance = new self();
+        if (isset($id)) {
+            $instance->setId($id)->read();
+        }
+        return $instance;
+    }
+
+    public function read() {
+        // ToDo from udvide
+    }
+
+    public function create() {
+        // ToDo new
+    }
+    public function update() {
+        // ToDo from udvide
+    }
+    public function deactivate() {
+        // ToDo from udvide
+    }
+    public function delete() {
+        // ToDo from udvide
+    }
+
+    /**
+     * Fills the target from an array
+     * @param array $data
+     */
+    public function set(array $data)
+    {
+        foreach ($data AS $key => $value) {
+            $this->__set($key, $value); // To use setters behind permission and type verification
+        }
+    }
+
+    /**
+     * Set via available Fluent Setter or return $this
+     * @param string $name
+     * @param mixed $value
+     * @return target
+     */
+    public function __set(string $name, $value):target {
+        switch($name) {
+            case 'id':
+                return $this->setID($value);
+            case 'owner':
+                return $this->setOwner($value);
+            case 'content':
+                return $this->setContent($value);
+            case 'xPos':
+                return $this->setXPos($value);
+            case 'yPos':
+                return $this->setYPos($value);
+            case 'map':
+                return $this->setMap($value);
+            default:
+                return $this;
+        }
+    }
+
+    /**
+     * Get via available Getter or return null
+     * @param string $name
+     * @return mixed
+     */
+    public function __get(string $name) {
+        switch($name) {
+            case 'id':
+                return $this->getID();
+            case 'owner':
+                return $this->getOwner();
+            case 'content':
+                return $this->getContent();
+            case 'xPos':
+                return $this->getXPos();
+            case 'yPos':
+                return $this->getYPos();
+            case 'map':
+                return $this->getMap();
+            default:
+                return null;
+        }
+    }
+
+    private function syncVWID() {
+        $sql = 'SELECT vw_id FROM udvide.targets WHERE t_id = ?';
+        $this->vw_id = access_DB::prepareExecuteFetchStatement($sql,[$this->id])[0]['vw_id'];
+    }
+
+    //<editor-fold desc="Fluent Setters with type and permission verification">
+
+    /**
+     * @param int $id
+     * @return target
+     */
+    public function setId(int $id = null): target
+    {
+        if (isset($id)) {
+            $this->id = $id;
+        }
+        return $this;
+    }
+
+    /**
+     * @param string $owner
+     * @return target
+     */
+    public function setOwner(string $owner = null): target
+    {
+        if (isset($owner)) {
+            $this->owner = $owner; // ToDo after User.php Refactor
+        }
+        return $this;
+    }
+
+    /**
+     * @param string $content
+     * @return target
+     */
+    public function setContent(string $content = null): target
+    {
+        if (isset($content)) {
+            $this->content = $content;
+        }
+        return $this;
+    }
+
+    /**
+     * @param int $xPos
+     * @return target
+     */
+    public function setXPos(int $xPos = null): target
+    {
+        if (isset($xPos)) {
+            $this->xPos = $xPos;
+        }
+        return $this;
+    }
+
+    /**
+     * @param int $yPos
+     * @return target
+     */
+    public function setYPos(int $yPos = null): target
+    {
+        if (isset($yPos)) {
+            $this->yPos = $yPos;
+        }
+        return $this;
+    }
+
+    /**
+     * @param string $map
+     * @return target
+     */
+    public function setMap(string $map = null): target
+    {
+        if (isset($map)) {
+            $this->map = $map; // ToDo after map.php
+        }
+        return $this;
+    }
+
+    /**
+     * @param resource|string $image
+     * @return target
+     */
+    public function setImage($image = null)
+    {
+        if (isset($image)) {
+            if (is_string($image)) {
+                $this->image =
+                    imagecreatefromstring(
+                        base64_decode(
+                            base64ImgToDecodeAbleBase64($image)
+                        )
+                    );
+            } else {
+                $this->image = $image;
+            }
+            $this->image = imgAssistant($this->image, ['maxFileSize' => VUFORIA_DATA_SIZE_LIMIT]);
+        }
+        return $this;
+    }
+
+    /**
+     * @param string $name
+     * @return target
+     */
+    public function setName(string $name = null): target
+    {
+        if (isset($name)) {
+            $this->name = purifyValue($name);
+        } elseif (empty($name)) {
+            // name is not allowed to be empty
+            // this solution trades 1:1000000 stability for ease and performance
+            $this->name = 'Anonymous Target '. random_int(1000000,9999999);
+        }
+        if (strlen($this->name) >= VUFORIA_TARGET_NAME_LIMIT) {
+            // name has a length limit
+            $this->name = substr($this->name, 0, VUFORIA_TARGET_NAME_LIMIT-3) . '...';
+        }
+        // we do ignore the case that name is both too long and not unique
+        return $this;
+    }
+
+    /**
+     * @param bool $active
+     * @return target
+     */
+    public function setActive(bool $active = null): target
+    {
+        if (isset($active)) {
+            $this->active = $active;
+        }
+        return $this;
+    }
+    //</editor-fold>
+
+    //<editor-fold desc="Getter with permission verification">
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOwner(): string
+    {
+        return $this->owner;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    /**
+     * @return int
+     */
+    public function getXPos(): int
+    {
+        return $this->xPos;
+    }
+
+    /**
+     * @return int
+     */
+    public function getYPos(): int
+    {
+        return $this->yPos;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMap(): string
+    {
+        return $this->map;
+    }
+
+    /**
+     * @return resource|string
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isActive(): bool
+    {
+        return $this->active;
+    }
+    //</editor-fold>
 }
