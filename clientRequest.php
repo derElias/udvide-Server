ea015a64a910d5fd16487f68fd4dbc344dc7dfb7
Janssen, Simon <simon.janssen@student.fhws.de>
2017-06-23 02:30:43 +0200

    endusers now cannot get deleted flagged targets

--- clientRequest.php
+++ clientRequest.php
@@ -12,5 +12,6 @@ header('Content-Type: application/text');
  * /clientRequest.php?t=[base64encodedTargetName]
  */
 $name = base64_decode($_GET['t']);
-$sql = 'SELECT content FROM udvide.Targets WHERE name = ?';
+$sql = 'SELECT content FROM udvide.Targets
+  WHERE name = ? AND deleted = 0 or deleted = false';
 return access_DB::prepareExecuteFetchStatement($sql,[$name])[0]['content'];
